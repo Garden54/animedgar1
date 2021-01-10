67d207da951fc4c47680b02e3bec9b07f0bbf67b
LudoGarden <ludovic.jardine@wanadoo.fr>
2021-01-10 20:09:26 +0100

    Initial Commit

---
+++ edgar_anim_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  edgar_anim_1
+//
+//  Created by Ludovic Jardiné on 10/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
